4e23fea7ffe5149ca66accbddc6ec09944c8bf8a
wu-qing-157 <wuqing157@gmail.com>
2020-12-23 11:30:54 +0800

    eval

--- eval.py
+++ eval.py
@@ -1,6 +1,5 @@
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-os.environ['CUDA_VISIBLE_DEVICES'] = '1,3'
 
 import tensorflow as tf
 config = tf.compat.v1.ConfigProto()
@@ -11,11 +10,12 @@ session = tf.compat.v1.Session(graph=tf.get_default_graph(),config=config)
 from tensorflow.keras import models, layers, datasets, utils, backend, optimizers, initializers
 backend.set_session(session)
 
-from keras_tqdm import TQDMCallback
+from tqdm.keras import TqdmCallback
+from tqdm import tqdm
 import numpy as np
 
 from cleverhans.utils_keras import KerasModelWrapper
-from cleverhans.attacks import FastGradientMethod
+from cleverhans.attacks import FastGradientMethod, MomentumIterativeMethod, DeepFool, LBFGS
 
 (Xtr, ytr), (Xts, yts) = datasets.cifar10.load_data()
 Xtr = Xtr.astype(np.float32)
@@ -23,12 +23,12 @@ Xts = Xts.astype(np.float32)
 ytr = utils.to_categorical(ytr)
 yts = utils.to_categorical(yts)
 CHILD_BATCH_SIZE = 64
-CHILD_EPOCHS = 1
+CHILD_EPOCHS = 120
 
 class TargetModel:
     def __init__(self, input_shape):
         self.model = self.create_model(input_shape)
-        optimizer = optimizers.SGD(decay=1e-3)
+        optilmizer = optimizers.SGD(decay=1e-3)
         self.model.compile(optimizer, 'categorical_crossentropy', ['accuracy'])
     def create_model(self, input_shape):
         x = input_layer = layers.Input(shape=input_shape)
@@ -47,7 +47,7 @@ class TargetModel:
             for i, subpolicy in enumerate(subpolicies):
                 X[which == i] = subpolicy(X[which == i])
         X = X.astype(np.float32) / 255
-        callback = TQDMCallback(leave_inner=False, leave_outer=False)
+        callback = TqdmCallback(leave=False, file=sys.stdout, verbose=0)
         callback.on_train_batch_begin = callback.on_batch_begin
         callback.on_train_batch_end = callback.on_batch_end
         self.model.fit(X, y, CHILD_BATCH_SIZE, CHILD_EPOCHS, verbose=0, callbacks=[callback])
@@ -61,38 +61,70 @@ if __name__ == "__main__":
 
     raw_wrap = KerasModelWrapper(raw.model)
     raw_fgsm_ = FastGradientMethod(raw_wrap, sess=session)
-    raw_fgsm_p = 0.5
-    raw_fgsm_eps = 0.2
-    def raw_fgsm(X):
+    def fgsm(X, which, prob, magn):
+        wrapped = FastGradientMethod(KerasModelWrapper(which.model), sess=session)
         X = X.copy()
         idx = np.random.uniform(size=len(X))
-        tensor = tf.convert_to_tensor(X[idx < raw_fgsm_p])
-        tensor = raw_fgsm_.generate(tf.convert_to_tensor(X[idx < raw_fgsm_p]), eps=raw_fgsm_eps)
-        # print(tensor)
-        # quit()
-        X[idx < raw_fgsm_p] = session.run(tensor)
+        idx = np.where(idx < prob)[0]
+        for i in tqdm(range(0, len(idx), CHILD_BATCH_SIZE), desc=f'batch: ', leave=False):
+            tensor = tf.convert_to_tensor(X[idx[i:i + CHILD_BATCH_SIZE]])
+            tensor = wrapped.generate(tensor, eps=0.1 * magn)
+            X[idx[i:i + CHILD_BATCH_SIZE]] = session.run(tensor)
+        return X
+    def df(X, which, prob, magn):
+        wrapped = DeepFool(KerasModelWrapper(which.model), sess=session)
+        X = X.copy()
+        idx = np.random.uniform(size=len(X))
+        idx = np.where(idx < prob)[0]
+        for i in tqdm(range(0, len(idx), CHILD_BATCH_SIZE), desc=f'batch: ', leave=False):
+            tensor = tf.convert_to_tensor(X[idx[i:i + CHILD_BATCH_SIZE]])
+            tensor = wrapped.generate(tensor, clip_min=0., clip_max=magn * 0.3 + 0.3)
+            X[idx[i:i + CHILD_BATCH_SIZE]] = session.run(tensor)
+        return X
+    def mim(X, which, prob, magn):
+        wrapped = MomentumIterativeMethod(KerasModelWrapper(which.model), sess=session)
+        X = X.copy()
+        idx = np.random.uniform(size=len(X))
+        idx = np.where(idx < prob)[0]
+        for i in tqdm(range(0, len(idx), CHILD_BATCH_SIZE), desc=f'batch: ', leave=False):
+            tensor = tf.convert_to_tensor(X[idx[i:i + CHILD_BATCH_SIZE]])
+            tensor = wrapped.generate(tensor, eps=0.1 * magn)
+            X[idx[i:i + CHILD_BATCH_SIZE]] = session.run(tensor)
+        return X
+    def lbfgs(X, which):
+        wrapped = LBFGS(KerasModelWrapper(which.model), sess=session)
+        X = X.copy()
+        for i in tqdm(range(0, len(X), CHILD_BATCH_SIZE), desc=f'batch: ', leave=False):
+            tensor = tf.convert_to_tensor(X[i:i + CHILD_BATCH_SIZE])
+            tensor = wrapped.generate(tensor, eps=0.1)
+            X[i:i + CHILD_BATCH_SIZE] = session.run(tensor)
         return X
 
-    best_subpolicies = [raw_fgsm]
-    attack_raw = raw_fgsm
-    attack_test = raw_fgsm
+    best_subpolicies = [
+        lambda X: df(df(X, test, 0.1, 0.6), test, 0.1, 0.7),
+        lambda X: mim(fgsm(X, test, 1.0, 0.1), test, 0.7, 0.4),
+        lambda X: df(fgsm(X, test, 0.1, 0.2), test, 0.9, 0.9),
+    ]
+    attack_raw = lambda X: lbfgs(X, raw)
+    attack_test = lambda X: lbfgs(X, test)
     print('\033[34mTrain raw model:\033[0m')
-    raw.fit(Xtr, ytr)
+    # raw.fit(Xtr, ytr)
     print('\033[34mTrain test model:\033[0m')
     test.fit(Xtr, ytr, subpolicies=best_subpolicies)
     print('\033[34mAttack test dataset:\033[0m')
-    Xat = attack_subpolicy(Xts)
+    Xraw = attack_raw(Xts)
+    Xtest = attack_test(Xts)
     print('\033[34mRaw  model on   raw    testcase:', end='')
     raw_raw = raw.evaluate(Xts, yts)
     print(f'\033[32m {raw_raw}\033[0m')
     print('\033[34mRaw  model on attacked testcase:', end='')
-    raw_attacked = raw.evaluate(Xat, yts)
+    raw_attacked = raw.evaluate(Xraw, yts)
     print(f'\033[32m {raw_attacked}\033[0m')
     print('\033[34mTest model on   raw    testcase:', end='')
     test_raw = raw.evaluate(Xts, yts)
     print(f'\033[32m {test_raw}\033[0m')
     print('\033[34mTest model on attacked testcase:', end='')
-    test_attacked = raw.evaluate(Xat, yts)
+    test_attacked = raw.evaluate(Xtest, yts)
     print(f'\033[32m {test_attacked}\033[0m')
     print(f'''
 Test result:
